d1be170f741413b4c1a7b32ac7f365559eba4168
Russell <russellcape98@gmail.com>
2020-12-22 16:42:30 -0500

    Learning how to use styled components

---
+++ src/components/DaughterTab.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DaughterTab() {
+
+    return (
+        <div>
+            <p>This is the second page.</p>
+        </div>
+    );
+    }
+
+    export default DaughterTab;
---
+++ src/components/MainTab.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MainTab() {
+
+    return (
+    <div>
+        <h2>
+            This is the first page.
+            <br />
+            Click on the button below.
+        </h2>
+        <Link to="/page2">
+            <button>
+                Go to Page 2 
+            </button>
+        </Link>
+    </div>
+    );
+}
+
+export default MainTab;
